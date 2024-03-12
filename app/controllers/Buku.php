1e0a338e8db645a0b42e9954d8496f8df9e26a95
azizrah15 <158453117+azizrah15@users.noreply.github.com>
2024-03-13 04:13:26 +0700

    tambah data buku

---
+++ app/controllers/Buku.php
@@ -0,0 +1,23 @@
+<?php
+
+class Buku extends Controller {
+
+    public function index() {
+        $data['judul'] = "Daftar Buku";
+        $data['buku'] = $this->model('Buku_model')->getAllBuku();
+        $this->view('templates/header', $data);
+        $this->view('buku/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function detail($id) {
+        $data['judul'] = "Detail Buku";
+        $data['buku'] = $this->model('Buku_model')->geBukubyId($id);
+        $this->view('templates/header', $data);
+        $this->view('buku/detail', $data);
+        $this->view('templates/footer');
+    }
+    public function tambah() {
+        
+    }
+}
--- app/models/Buku_model.php
+++ app/models/Buku_model.php
@@ -1,2 +1,30 @@
 <?php
 
+class Buku_model {
+    private $table = 'Buku';
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
+    public function getAllBuku() {
+        $this->db->query("SELECT * FROM".$this->table);
+        return $this->db->resultSet();
+    }
+    public function getBukuByid($id) {
+        $this->db->query("SELECT * FROM".$this->table."WHERE idbuku = :id");
+    }
+
+    public function tambahBuku($data) {
+        $query = "INSERT INTO buku
+                    VALUES(null, :judul, :penulis, :penerbit, :tahun terbit)";
+        $this->db->query($query);
+        $this->db->bind('judul', $data['judul']);
+        $this->db->bind('penulis', $data['penulis']);
+        $this->db->bind('penerbit', $data['penerbit']);
+        $this->db->bind('tahun terbit', $data['tahun terbit']);
+
+        $this->db->execute();
+    }
+}
