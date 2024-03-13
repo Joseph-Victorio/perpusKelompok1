84c1c2347f660769005c28ad2fca740fbafef846
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2024-03-13 11:46:42 +0700

    change style img index

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,5 @@
 
-<div class="d-flex p-4 gap-4 bg-merah-pastel  ">
+<div class="d-flex p-4 gap-4 bg-merah-pastel align-items-center ">
         <!-- KIRI -->
         <!-- GAMBAR -->
     <div class="img-container w-75 ">
@@ -7,10 +7,14 @@
     </div>
         <!-- KANAN -->
     <div>
-        <h1>Kumpulan Buku Programming Terbaik</h1>
+        <h1 class="text-blue fw-bold">Kumpulan Buku Programming Terbaik</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit fuga porro excepturi deserunt nihil optio corrupti consequatur placeat velit perspiciatis.
+        </p>
+        <button class=" btn-lebih-lanjut">Cari Lebih Lanjut</button>
     </div>
 </div>
-<img src="<?=BASEURL?>/img/waves.svg" class="w-100" alt="">
+<img src="<?=BASEURL?>/img/curve.svg" class="w-100" alt="">
 
 <div>
     
