565f9373a10a194e5947b031928640dd3a257b7c
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2024-03-13 15:39:55 +0700

    updated homePage

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -11,11 +11,56 @@
         <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit fuga porro excepturi deserunt nihil optio corrupti consequatur placeat velit perspiciatis.
         </p>
-        <button class=" btn-lebih-lanjut">Cari Lebih Lanjut</button>
+        <button class="btnKu">Cari Lebih Lanjut</button>
     </div>
 </div>
-<img src="<?=BASEURL?>/img/curve.svg" class="w-100" alt="">
-
-<div>
-    
+<img src="<?=BASEURL?>/img/curve.svg" class="w-100 z-1" alt="">
+    <!-- Search Books -->
+<div class="w-50 mx-auto z-2 position-absolute translate-middle-y top-100 start-0 end-0 bottom-0 mb-5">
+    <form class="form-inline my-2  my-lg-0 d-flex gap-2 top-100 ">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search book" aria-label="Search">
+        <button class="btnKu my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</div>
+<br>
+<br>
+<!-- Book Category -->
+<div class="category text-blue mt-5 mb-5">
+    <h2 class="text-center">Category Buku</h2>
+</div>
+<div class="d-flex p-4 m-4 justify-content-center gap-4 fw-bold">
+    <div class="category-card p-2 d-flex flex-col">
+        <a href="">
+            <img src="" alt="">
+            HTML
+        </a>
+    </div>
+    <div class="category-card p-2  ">
+        <a href="" class="d-flex flex-column gap-2 align-items-center">
+            <div class="w-25">
+                <img class="w-100" src="https://www.php.net//images/logos/new-php-logo.svg" alt="">
+            </div>
+            <p>PHP</p>
+        </a>
+    </div>
+    <div class="category-card p-2 d-flex flex-col">
+        
+        <a href="">
+            <img src="" alt="">
+            Python
+        </a>
+    </div>
+    <div class="category-card p-2 d-flex flex-col">
+        
+        <a href="">
+            <img src="" alt="">
+            Javascript
+        </a>
+    </div>
+    <div class="category-card p-2 d-flex flex-col">
+        <a href="">
+            <img src="" alt="">
+            CSS
+        </a>
+    </div>
 </div>
