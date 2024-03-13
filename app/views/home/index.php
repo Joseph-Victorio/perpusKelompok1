1db6a531bf8385ae1ce189df7e61aa356633fc62
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2024-03-13 23:12:59 +0700

    add book covers

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -42,31 +42,31 @@
         </div>
         <div class="card p-2 bg-merah-pastel" style="width: 18rem;">
             <div class="img-container w-75 mx-auto">
-                <img src="https://i.pinimg.com/564x/76/66/84/7666840a23cded4a54f7c0c089bce761.jpg" class="card-img-top w-100 " alt="...">
+                <img src="https://i.pinimg.com/564x/c3/5e/26/c35e266ac26da3b18e5fb0928685e0a0.jpg" class="card-img-top w-100 " alt="...">
             </div>
         <div class="card-body">
-            <h5 class="card-title">Waves</h5>
-            <p class="card-text">Ingrid Chabbert & Carole Maurel</p>
+            <h5 class="card-title">The Fall</h5>
+            <p class="card-text">Jared Muralt</p>
             
         </div>
         </div>
         <div class="card p-2 bg-merah-pastel" style="width: 18rem;">
             <div class="img-container w-75 mx-auto">
-                <img src="https://i.pinimg.com/564x/76/66/84/7666840a23cded4a54f7c0c089bce761.jpg" class="card-img-top w-100 " alt="...">
+                <img src="https://i.pinimg.com/564x/43/ad/53/43ad53d413896806a308c22fb09a1708.jpg" class="card-img-top w-100 " alt="...">
             </div>
         <div class="card-body">
-            <h5 class="card-title">Waves</h5>
-            <p class="card-text">Ingrid Chabbert & Carole Maurel</p>
+            <h5 class="card-title">If You Could See The Sun</h5>
+            <p class="card-text">Ann Liang</p>
             
         </div>
         </div>
         <div class="card p-2 bg-merah-pastel" style="width: 18rem;">
             <div class="img-container w-75 mx-auto">
-                <img src="https://i.pinimg.com/564x/76/66/84/7666840a23cded4a54f7c0c089bce761.jpg" class="card-img-top w-100 " alt="...">
+                <img src="https://i.pinimg.com/564x/f4/9b/f4/f49bf422414a1e49337b245634459f19.jpg" class="card-img-top w-100 " alt="...">
             </div>
         <div class="card-body">
-            <h5 class="card-title">Waves</h5>
-            <p class="card-text">Ingrid Chabbert & Carole Maurel</p>
+            <h5 class="card-title">The Crimes of Steamfield</h5>
+            <p class="card-text">Alberto Rey</p>
             
         </div>
         </div>
