b635d5381ecbed7e0380417cf15fa8f92ae6a87c
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2024-03-13 07:41:25 +0700

    hapus css ga kepake

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,11 +1,12 @@
 <div class="container">
-    <div class="jumbotron">
-        <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">Nama: <?= $data['nama']; ?></p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="<?=BASEURL?>about" role="button">Learn more</a>
-        </p>
+    <div class="d-flex p-4 gap-4 ">
+        <!-- KIRI -->
+        <!-- GAMBAR -->
+        <div class="img-container">
+            <img src="" alt="">
+            a
+        </div>
+        <!-- KANAN -->
+        <div>a</div>
     </div>
 </div>
