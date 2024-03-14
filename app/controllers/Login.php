9ee7dab7cfefea3001c676d830fe3544477dca53
azizrah15 <azizrahmansyah15@gmail.com>
2024-03-14 20:42:03 +0700

    buku done, login setengah

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -4,9 +4,10 @@ class Login extends Controller {
     public function index() {
             $data['judul']="Login";
             $data['nama'] = $this->model('User_model')->getUser();
+
            
             $this->view('login/index', $data);
-            
+            $this->view('templates/footer');
     }
 
 }
