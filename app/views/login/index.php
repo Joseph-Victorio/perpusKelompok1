3dc015edc9cd2603ff80fa584377b06aa0081864
azizrah15 <azizrahmansyah15@gmail.com>
2024-03-17 00:08:38 +0700

    mvc login done

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login Admin </title>
-    <link rel="stylesheet" href="../public/css/login.css">
-    <link rel="stylesheet" href="<?=BOOTSTRAP?>">
-    <link rel="stylesheet" href="<?=BASEURL?>/css/style.css">
-  </head>
-  <body>
-    <div class="center">
+
+<head>
+  <meta charset="utf-8">
+  <title>Login Admin </title>
+  <link rel="stylesheet" href="../public/css/login.css">
+  <link rel="stylesheet" href="<?= BOOTSTRAP ?>">
+  <link rel="stylesheet" href="<?= BASEURL ?>/css/style.css">
+</head>
+
+<body>
+  <div class="center">
     <div class="img-container w-75 ">
-        <img src="<?=BASEURL?>/img/booksWithLogoCS.png" class="w-100" alt="" sizes="32x32">
+      <img src="<?= BASEURL ?>/img/booksWithLogoCS.png" class="w-100" alt="" sizes="32x32">
     </div>
-      <h1>Login Admin </h1>
-      <form method="post">
-        <div class="txt_field">
-          <input type="text" required>
-          <span></span>
-          <label>Username</label>
-        </div>
-        <div class="txt_field">
-          <input type="password" required>
-          <span></span>
-          <label>Password</label>
-        </div>
-        <input type="submit" value="Login">
-        <br></br>
-    </div>
-      </form>
-    </div>
-    <img src="<?=BASEURL?>/img/curve.svg" class="w-100 z-1" alt="">
-  </body>
+    <h1>Login Admin </h1>
+    <form method="post" action="login/processLogin">
+      <div class="txt_field">
+        <input type="text" required name="username">
+        <span></span>
+        <label>Username</label>
+      </div>
+      <div class="txt_field">
+        <input type="password" required name="password">
+        <span></span>
+        <label>Password</label>
+      </div>
+      <input type="submit" value="Login">
+      <br></br>
+  </div>
+  </form>
+  </div>
+  <img src="<?= BASEURL ?>/img/curve.svg" class="w-100 z-1" alt="">
+</body>
+
 </html>
